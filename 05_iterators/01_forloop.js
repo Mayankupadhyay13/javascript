bba6735d4da8d777a62829864988cd772fec4e22
Mayank Upadhyay <113622780+Mayankupadhyay13@users.noreply.github.com>
2024-08-24 12:48:39 +0000

    for

---
+++ 05_iterators/01_forloop.js
@@ -0,0 +1,20 @@
+// for 
+for (let index = 0; index < 10; index++) {
+    const element = index;
+  //  console.log(element)
+    
+}
+for (let i = 1; i <= 10; i++) {
+    const element = i;
+    console.log(`Outer loop value: ${i}`)
+    for (j = 1; j <=10; j++) {
+        const element = j
+        console.log(`${i} * ${j} =`+ i*j );
+        
+    }
+}
+
+// break and continue 
+// break , breaks the loop and cotinue skip the matching iteration 
+//and below code and by incrementing continue the loop 
+
