7e90bd34e39e451c24158f2ce41860167e1bfed5
Mayank Upadhyay <113622780+Mayankupadhyay13@users.noreply.github.com>
2024-08-24 17:33:23 +0000

    while do_while

---
+++ 05_iterators/02_while_doWhile.js
@@ -0,0 +1,21 @@
+let index =0
+// while (index<=10) {
+//     console.log(index)
+//     index = index+2
+// }
+
+// with array//
+let myarr = ['flash','batman','superman']
+let arr = 0
+while (arr < myarr.length) {
+    console.log(`value is ${myarr[arr]}`);
+    arr++
+}
+
+// do_while
+let score = 6
+do {
+    console.log(score);
+    score++
+    
+} while (score<=5);
