d602d56f278ad41610fead576932d07929e432a6
Mayank Upadhyay <113622780+Mayankupadhyay13@users.noreply.github.com>
2024-09-04 19:45:39 +0000

    project1

---
+++ 07_projects/p1/script.js
@@ -0,0 +1,32 @@
+const color = document.querySelectorAll('.button');
+//console.log(color);
+const body = document.querySelector('body');
+
+color.forEach(function (button) {
+  console.log(button);
+  button.addEventListener('click', function (e) {
+    console.log(e);
+    console.log(e.target);
+    if (e.target.id === 'grey') {
+      body.style.backgroundColor = e.target.id;
+    }
+    if (e.target.id === 'red') {
+      body.style.backgroundColor = e.target.id;
+    }
+    if (e.target.id === 'blue') {
+      body.style.backgroundColor = e.target.id;
+    }
+    if (e.target.id === 'yellow') {
+      body.style.backgroundColor = e.target.id;
+    }
+    if (e.target.id === 'purple') {
+      body.style.backgroundColor = e.target.id;
+    }
+    if (e.target.id === 'skyblue') {
+      body.style.backgroundColor = e.target.id;
+    }
+    if (e.target.id === 'green') {
+      body.style.backgroundColor = e.target.id;
+    }
+  });
+});
